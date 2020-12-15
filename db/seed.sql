d3066ae00b28bc662c4172ed036e6e14c70f475e
Brooke Miller <brookecm28@gmail.com>
2020-12-15 09:56:51 -0700

    SQL seed set up

---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+CREATE TABLE helo_users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(150) NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    profile_pic TEXT
+);
+
+CREATE TABLE helo_posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(45) NOT NULL,
+    content TEXT,
+    img TEXT,
+    author_id INTEGER,
+    date_created TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES helo_users(id)
+)
